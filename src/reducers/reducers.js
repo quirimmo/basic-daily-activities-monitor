0b5a87915d1f729ea91f4e40ec3a2f0c10753d96
Quirino Lo Russo <quirimmo@gmail.com>
2018-12-11 20:35:12 +0100

    added reducer

--- src/reducers/reducers.js
+++ src/reducers/reducers.js
@@ -1,4 +1,6 @@
 import { combineReducers } from 'redux';
 import { items } from './items.reducer';
+import { error } from './error.reducer';
+import { isLoading } from './loading.reducer';
 
-export const reducers = combineReducers({ items });
+export const reducers = combineReducers({ items, error, isLoading });
